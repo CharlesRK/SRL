e6b19f10a6f8df5c5eeb08e380f284861eb10cea
Charles <32884698+CharlesRK@users.noreply.github.com>
2024-02-14 14:49:19 -0500

    Add files via upload

---
+++ Go1 Unitree Robot Dog/Sheriloye/squarev3.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import sys
+import time
+import math
+
+sys.path.append('../lib/python/amd64')
+import robot_interface as sdk
+
+
+if __name__ == '__main__':
+
+    HIGHLEVEL = 0xee
+    LOWLEVEL  = 0xff
+
+    udp = sdk.UDP(HIGHLEVEL, 8080, "192.168.123.161", 8082)
+
+    cmd = sdk.HighCmd()
+    state = sdk.HighState()
+    udp.InitCmdData(cmd)
+
+    motiontime = 0
+    while True:
+
+
+        udp.Recv()
+        udp.GetRecv(state)
+        
+        # print(motiontime)
+        # print(state.imu.rpy[0])
+        # print(motiontime, state.motorState[0].q, state.motorState[1].q, state.motorState[2].q)
+        # print(state.imu.rpy[0])
+
+        cmd.mode = 0      # 0:idle, default stand      1:forced stand     2:walk continuously
+        cmd.gaitType = 0
+        cmd.speedLevel = 0
+        cmd.footRaiseHeight = 0
+        cmd.bodyHeight = 0
+        cmd.euler = [0, 0, 0]
+        cmd.velocity = [0, 0]
+        cmd.yawSpeed = 0.0
+        cmd.reserve = 0
+
+        # cmd.mode = 2
+        # cmd.gaitType = 1
+        # # cmd.position = [1, 0]
+        # # cmd.position[0] = 2
+        # cmd.velocity = [-0.2, 0] # -1  ~ +1
+        # cmd.yawSpeed = 0
+        # cmd.bodyHeight = 0.1
+        
+        print(f"starting:\n")
+        for i in range(4):
+            motiontime = motiontime + 1
+            print(motiontime)
+            time.sleep(0.002)
+
+            if(motiontime > 1000 and motiontime < 3500):
+                cmd.mode = 2
+                cmd.gaitType = 1
+                cmd.velocity = [0.4, 0] # -1  ~ +1
+                cmd.yawSpeed = 0
+                cmd.footRaiseHeight = 0.1
+                # printf("walk\n")
+            
+            if(motiontime > 3500 and motiontime < 4500):
+                cmd.mode = 0
+                cmd.velocity = [0, 0]
+            
+            if(motiontime > 4500 and motiontime < 5000):
+                cmd.mode = 2
+                cmd.gaitType = 1
+                cmd.velocity = [0.4, 0] # -1  ~ +1
+                cmd.yawSpeed = 2
+                cmd.footRaiseHeight = 0.1
+                # printf("walk\n")
+            if(motiontime > 5000 and motiontime < 5500):
+                cmd.mode = 0
+                cmd.velocity = [0, 0]
+            
+            udp.SetSend(cmd)
+            udp.Send()
+            motiontime = 0
